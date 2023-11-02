34aed10efecf1100b3aaa1762f1375972d0a23ec
eng-awais <devawais.cs@gmail.com>
2023-11-02 12:54:12 +0500

    Added search route

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,13 @@
 class SearchesController < ApplicationController
-  def index
-    render json: GithubApi.call(searches_params[:query], sort: searches_params[:sort], order: searches_params[:order])
+  def show
+    render json: GithubApi.call(search_params[:query], sort: search_params[:sort], order: search_params[:order])
   rescue ArgumentError => e
     render json: { error: e.message }, status: :bad_request
   end
 
   private
 
-  def searches_params
+  def search_params
     params.permit(:query, :sort, :order)
   end
 end
